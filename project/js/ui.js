03d171497fe11af393b15524c7a94bce7398c7cc
JJFighting <it.deng@foxmail.com>
2018-06-06 01:42:05 +0800

    幻灯片实现

--- project/js/ui.js
+++ project/js/ui.js
@@ -60,6 +60,83 @@ $.fn.UiBackTop = function(){
 	})
 }
 
+/**
+ * 1.左右箭头需要能控制翻页
+ * 2.翻页的时候，进度点，要联动进行focus
+ * 3.翻到第三页的时候，下一页需要回到 第一页，翻到第一页的时候，同理
+ * 4.进度点，在点击的时候，需要切换到对应的页面
+ * 5.没有(进度点击，翻页操作) 的时候需要进行自动滚动
+ * 6.滚动过程中，屏蔽其他操作（自动滚动，左右翻页，进度点点击）
+ * 7.高级-无缝滚动
+ */
+$.fn.uiSlider = function(){
+
+	var ui = $(this);
+	var wrap = $('.ui-slider-wrap',ui);
+	// console.log('内容框架:'+wrap);
+	var goPrev = $('.ui-slider-arrow .left',ui);
+	// console.log(goPrev);
+	var goNext = $('.ui-slider-arrow .right',ui);
+	// console.log(goNext);
+	var items  = $('.ui-slider-wrap .item',ui);
+	var tips   = $('.ui-slider-process .item',ui);
+	
+	// 预定义
+	var current = 0;
+	var size    = items.size();
+	var width   = items.eq(0).width();
+	var enableAuto = true;
+
+	ui
+	.on('mouseover',function(){
+		enableAuto = false;
+	})
+	.on('mouseout',function(){
+		enableAuto = true;
+	})
+
+	wrap
+	.on('move_prev',function(){
+		if(current <= 0){
+			current = size;
+		}
+		current -= 1;
+		console.log(current);
+		wrap.triggerHandler('move_to',current);
+	})
+	.on('move_next',function(){
+		if(current >= size-1){
+			current = -1;
+		}
+		current += 1;
+		console.log(current);
+		wrap.triggerHandler('move_to',current);
+	})
+	.on('move_to',function(evt,index){
+		wrap.css('left',index*width*-1);
+		tips.removeClass('item_focus').eq(index).addClass('item_focus');
+	})
+	.on('auto_move',function(){
+		setInterval(function(){
+			enableAuto && wrap.triggerHandler('move_next')
+		},2000)
+	})
+	.triggerHandler('auto_move');
+
+	goPrev.on('click',function(){
+		console.log('shangyige');
+		wrap.triggerHandler('move_prev');
+	});
+	goNext.on('click',function(){
+		wrap.triggerHandler('move_next');
+	});
+	tips.on('click',function(){
+		var index = $(this).index();
+		wrap.triggerHandler('move_to',index);
+	})
+
+}
+
 // 页面的脚本逻辑
 $(function(){
 	$('.ui-search').UiSearch();
@@ -68,4 +145,6 @@ $(function(){
 	$('.content-tab .block .item').UiTab('.block-caption > a','.block-content > .block-wrap','block-caption-');
 
 	$('body').UiBackTop();
+
+	$('.ui-slider').uiSlider();
 })
